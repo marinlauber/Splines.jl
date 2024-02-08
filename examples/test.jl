4f211b3a5bd7a23406780fa806fbd6d843a168bb
marinlauber <marinlauber@gmail.com>
2024-02-08 16:15:05 +0100

    add dynamic relaxation solver

---
+++ examples/test.jl
@@ -0,0 +1,37 @@
+using Splines
+using Plots
+
+numElem=2;degP=3
+
+println("Testing on fixed-fixed beam with UDL:")
+println(" numElem: ", numElem)
+println(" degP: ", degP)
+# Material properties and mesh
+ptLeft = 0.0
+ptRight = 1.0
+EI = 1.0
+EA = 1.0
+exact_sol(x) = 1.0/(24EI).*(x .- 2x.^3 .+ x.^4) # pinned - pinned
+
+mesh, gauss_rule = Mesh1D(ptLeft, ptRight, numElem, degP)
+
+# boundary conditions
+Dirichlet_BC = []; Neumann_BC = []
+
+# make a problem
+operator = DynamicFEOperator(mesh, gauss_rule, EI, EA, Dirichlet_BC, Neumann_BC)
+
+# uniform external loading at integration points
+force = zeros(2,length(uv_integration(operator))); force[2,:] .= 1
+
+result = relaxation!(operator,[1,5,6,10])
+
+u0 = result[1:mesh.numBasis]
+w0 = result[mesh.numBasis+1:2mesh.numBasis]
+x = LinRange(ptLeft, ptRight, numElem+1)
+u = x .+ getSol(mesh, u0, 1)
+w = getSol(mesh, w0, 1)
+we = exact_sol(x)
+println("Error: ", norm(w .- we))
+Plots.plot(u, w, label="Sol")
+Plots.plot!(x, we, label="Exact")
--- src/Solver.jl
+++ src/Solver.jl
@@ -3,77 +3,77 @@ using ImplicitAD
 using LineSearches
 using StaticArrays
 
-struct GeneralizedAlpha
-    op :: StaticFEOperator
-    u ::Union{AbstractVector,Tuple{Vararg{AbstractVector}}}
-    αm :: Float64
-    αf :: Float64
-    β :: Float64
-    γ :: Float64
-    Δt :: Vector{Float64}
-    function GeneralizedAlpha(opr::StaticFEOperator; ρ∞=1.0)
-        αm = (2.0 - ρ∞)/(ρ∞ + 1.0);
-        αf = 1.0/(1.0 + ρ∞)
-        γ = 0.5 - αf + αm;
-        β = 0.25*(1.0 - αf + αm)^2;
-        new(opr,(zero(opr.resid),zero(opr.resid),zero(opr.resid)),αm,αf,β,γ,[0.0])
-    end
-end
+# struct GeneralizedAlpha
+#     op :: StaticFEOperator
+#     u ::Union{AbstractVector,Tuple{Vararg{AbstractVector}}}
+#     αm :: Float64
+#     αf :: Float64
+#     β :: Float64
+#     γ :: Float64
+#     Δt :: Vector{Float64}
+#     function GeneralizedAlpha(opr::StaticFEOperator; ρ∞=1.0)
+#         αm = (2.0 - ρ∞)/(ρ∞ + 1.0);
+#         αf = 1.0/(1.0 + ρ∞)
+#         γ = 0.5 - αf + αm;
+#         β = 0.25*(1.0 - αf + αm)^2;
+#         new(opr,(zero(opr.resid),zero(opr.resid),zero(opr.resid)),αm,αf,β,γ,[0.0])
+#     end
+# end
 
-"""
-    solve_step!(solver::GeneralizedAlpha,force::AbstractArray,Δt::Float64)
+# """
+#     solve_step!(solver::GeneralizedAlpha,force::AbstractArray,Δt::Float64)
 
-steps a (potentially nonlinear) operator forward in time, given an external
-force and a time step size.
-"""
-function solve_step!(solver::GeneralizedAlpha, force, Δt)
+# steps a (potentially nonlinear) operator forward in time, given an external
+# force and a time step size.
+# """
+# function solve_step!(solver::GeneralizedAlpha, force, Δt)
               
-    # useful
-    αf, αm, β, γ = solver.αf, solver.αm, solver.β, solver.γ
+#     # useful
+#     αf, αm, β, γ = solver.αf, solver.αm, solver.β, solver.γ
 
-    # structural time steps
-    tⁿ   = sum(solver.Δt[end])
-    tⁿ⁺ᵅ = αf*(tⁿ+Δt) + (1.0-αf)*tⁿ;
+#     # structural time steps
+#     tⁿ   = sum(solver.Δt[end])
+#     tⁿ⁺ᵅ = αf*(tⁿ+Δt) + (1.0-αf)*tⁿ;
 
-    # predictor (initial guess) for the Newton-Raphson scheme
-    (dⁿ⁺¹, vⁿ⁺¹, aⁿ⁺¹) = solver.u
-    dⁿ=copy(dⁿ⁺¹); vⁿ=copy(vⁿ⁺¹); aⁿ=copy(aⁿ⁺¹);
+#     # predictor (initial guess) for the Newton-Raphson scheme
+#     (dⁿ⁺¹, vⁿ⁺¹, aⁿ⁺¹) = solver.u
+#     dⁿ=copy(dⁿ⁺¹); vⁿ=copy(vⁿ⁺¹); aⁿ=copy(aⁿ⁺¹);
 
-    # Newton-Raphson iterations loop
-    r₂ = 1.0; iter = 1;
-    while r₂ > 1.0e-6 && iter < 1000
+#     # Newton-Raphson iterations loop
+#     r₂ = 1.0; iter = 1;
+#     while r₂ > 1.0e-6 && iter < 1000
 
-        # compute v_{n+1}, a_{n+1}, ... from "Isogeometric analysis: toward integration of CAD and FEA"
-        vⁿ⁺¹ = γ/(β*Δt)*dⁿ⁺¹ - γ/(β*Δt)*dⁿ + (1.0-γ/β)*vⁿ - Δt*(γ/2β-1.0)*aⁿ;
-        aⁿ⁺¹ = 1.0/(β*Δt^2)*dⁿ⁺¹ - 1.0/(β*Δt^2)*dⁿ - 1.0/(β*Δt)*vⁿ - (1.0/2β-1.0)*aⁿ;
+#         # compute v_{n+1}, a_{n+1}, ... from "Isogeometric analysis: toward integration of CAD and FEA"
+#         vⁿ⁺¹ = γ/(β*Δt)*dⁿ⁺¹ - γ/(β*Δt)*dⁿ + (1.0-γ/β)*vⁿ - Δt*(γ/2β-1.0)*aⁿ;
+#         aⁿ⁺¹ = 1.0/(β*Δt^2)*dⁿ⁺¹ - 1.0/(β*Δt^2)*dⁿ - 1.0/(β*Δt)*vⁿ - (1.0/2β-1.0)*aⁿ;
 
-        # compute d_{n+af}, v_{n+af}, a_{n+am}, ...
-        dⁿ⁺ᵅ = αf*dⁿ⁺¹ + (1.0-αf)*dⁿ;
-        vⁿ⁺ᵅ = αf*vⁿ⁺¹ + (1.0-αf)*vⁿ;
-        aⁿ⁺ᵅ = αm*aⁿ⁺¹ + (1.0-αm)*aⁿ;
+#         # compute d_{n+af}, v_{n+af}, a_{n+am}, ...
+#         dⁿ⁺ᵅ = αf*dⁿ⁺¹ + (1.0-αf)*dⁿ;
+#         vⁿ⁺ᵅ = αf*vⁿ⁺¹ + (1.0-αf)*vⁿ;
+#         aⁿ⁺ᵅ = αm*aⁿ⁺¹ + (1.0-αm)*aⁿ;
 
-        # update the jacobian, the residual and the external force
-        integrate!(solver.op, dⁿ⁺ᵅ, force)
+#         # update the jacobian, the residual and the external force
+#         integrate!(solver.op, dⁿ⁺ᵅ, force)
 
-        # compute the jacobian and the residuals
-        solver.op.jacob .= αm/(β*Δt^2)*solver.op.mass .+ αf*solver.op.jacob
-        solver.op.resid .= solver.op.stiff*dⁿ⁺ᵅ + solver.op.mass*aⁿ⁺ᵅ - solver.op.ext
+#         # compute the jacobian and the residuals
+#         solver.op.jacob .= αm/(β*Δt^2)*solver.op.mass .+ αf*solver.op.jacob
+#         solver.op.resid .= solver.op.stiff*dⁿ⁺ᵅ + solver.op.mass*aⁿ⁺ᵅ - solver.op.ext
 
-        # apply BC
-        applyBC!(solver.op)
+#         # apply BC
+#         applyBC!(solver.op)
 
-        # check convergence
-        r₂ = norm(solver.op.resid);
-        if r₂ < 1.0e-6 && break; end
+#         # check convergence
+#         r₂ = norm(solver.op.resid);
+#         if r₂ < 1.0e-6 && break; end
 
-        # newton solve for the displacement increment
-        dⁿ⁺¹ -= solver.op.jacob\solver.op.resid; iter += 1
-    end
-    # save variables
-    solver.u[1] .= dⁿ⁺¹
-    solver.u[2] .= vⁿ⁺¹
-    solver.u[3] .= aⁿ⁺¹
-end
+#         # newton solve for the displacement increment
+#         dⁿ⁺¹ -= solver.op.jacob\solver.op.resid; iter += 1
+#     end
+#     # save variables
+#     solver.u[1] .= dⁿ⁺¹
+#     solver.u[2] .= vⁿ⁺¹
+#     solver.u[3] .= aⁿ⁺¹
+# end
 
 function lsolve!(op::AbstractFEOperator, force)
 
@@ -178,4 +178,41 @@ function solve_step!(op::DynamicFEOperator, force, Δt)
     op.u[1] .= dⁿ⁺¹
     op.u[2] .= vⁿ⁺¹
     op.u[3] .= aⁿ⁺¹
+end
+
+
+function relaxation!(op::AbstractFEOperator, BC=[]; ρ::Function=i->1.0, Δt=0.01, N=10_000)
+
+    mass = zero(op.stiff)
+    global_mass!(mass, op.mesh, ρ, op.gauss_rule)
+    for i ∈ BC
+        mass[:,i] .= 0.
+        mass[i,:] .= 0.
+        mass[i,i] = 1.0
+    end
+    v = zero(op.x)
+    accel = zero(op.x)
+    for it ∈ 1:N
+        
+        integrate!(op, op.x, force)
+        # get force delta
+        op.resid .= op.stiff*op.x - op.ext
+        # apply BC on the acceleration
+        op.resid[BC] .= 0.0
+
+        # compute acceleration
+        accel .= op.mass\op.resid
+        
+        # velocity relaxed
+        v .= 0.99*v .+ accel*Δt;
+        
+        # Update Positions
+        delta_u = (v.*Δt)./2;
+        delta_u[BC] .= 0.0
+
+        #Total displacements
+        op.x .+= delta_u
+        norm(delta_u)<√eps() && break
+    end
+    return op.x
 end
